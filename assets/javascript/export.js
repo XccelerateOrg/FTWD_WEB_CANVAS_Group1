401447de7e3ca4a50998e6882c2cd737e6586373
Zach Lalwani <zach.lalwani@gmail.com>
2021-06-23 16:26:59 +0800

    toolBar Icons and ToolTip

--- assets/javascript/export.js
+++ assets/javascript/export.js
@@ -1,7 +1,15 @@
-function download() {
-    var download = document.getElementById("export-btn");
+function downloadPNG() {
+    var download = document.getElementById("export-btnPNG");
     var image = document.getElementById("canvas").toDataURL("image/png")
         .replace("image/png", "image/octet-stream");
     download.setAttribute("href", image);
    
     }
+function downloadJPG() {
+    var download = document.getElementById("export-btnJPG");
+    var image = document.getElementById("canvas").toDataURL("image/jpg")
+        .replace("image/jpg", "image/octet-stream");
+    download.setAttribute("href", image);
+   
+    }
+
