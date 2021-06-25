2372e5e0a4857777606e0c390fa3a0d781d06898
Zach Lalwani <zach.lalwani@gmail.com>
2021-06-25 13:07:29 +0800

    stamp

---
+++ assets/javascript/Stamp/stampTroll.js
@@ -0,0 +1,23 @@
+class StampTroll extends PaintFunction {
+    constructor(ctx, ctxDraft) {
+        super();
+        this.ctx = ctx;
+
+    }
+
+
+
+    
+
+
+    onMouseDown(coord, e) {
+        var trollimg = new Image();
+        trollimg.src = "http://www.redspiderfish.com/skoolprojects/aid/img/angry_troll_face.png";//set the link
+
+
+        this.ctx.drawImage(trollimg, coord[0], coord[1]);
+        console.log("stamp");
+
+
+    }
+}
