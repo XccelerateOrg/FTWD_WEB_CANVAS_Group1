0ec71dc354e51f002b7f73d9ce8220e18b2a6038
ih810 <ivanhong810@gmail.com>
2021-06-26 12:40:14 +0800

    bug fixed

--- assets/javascript/undoRedo.js
+++ assets/javascript/undoRedo.js
@@ -1,34 +1,38 @@
 function undo() {
-    console.log(`undo work`)
-    if(index >= 0){
-        // console.log(`undo if state`, index)
-        redoArray.push(restoreArray.pop());
-        // console.log(`resotre`, restoreArray)
-        reIndex +=1;
-        index -= 1;
-        if (index < 0) {
-            // console.log(`index < 0`)
-            ctx.clearRect(0, 0, canvas.width, canvas.height)
-        } else {
-            // console.log(`index > 0`)
-            ctx.putImageData(restoreArray[index], 0, 0);
-            // console.log(restoreArray[index])
-            // console.log(`index > 0 done`)
-        };
+  console.log(`undo work`);
+  if (index >= 0) {
+    // console.log(`undo if state`, index)
+    redoArray.push(restoreArray.pop());
+    // console.log(`resotre`, restoreArray)
+    reIndex += 1;
+    index -= 1;
+    if (index < 0) {
+      // console.log(`index < 0`)
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
     } else {
-        redoArray = [];
-        index = -1;
+      // console.log(`index > 0`)
+      ctx.putImageData(restoreArray[index], 0, 0);
+      // console.log(restoreArray[index])
+      // console.log(`index > 0 done`)
     }
+  } else {
+    redoArray = [];
+    index = -1;
+  }
 }
 
-function redo(){
-    if (reIndex > -1){
-        ctx.putImageData(redoArray[reIndex],0 ,0);
-        restoreArray.push(redoArray.pop());
-        reIndex -= 1;
-        index += 1;
-    } else {
-        restoreArray = [];
-        reIndex = -1
+function redo() {
+  if (reIndex > -1) {
+    console.log(redoArray[reIndex]);
+    console.log(reIndex)
+    ctx.putImageData(redoArray[reIndex], 0, 0);
+    restoreArray.push(redoArray.pop());
+    if (reIndex > 0) {
+      reIndex -= 1;
     }
-} 
+    index += 1;
+  } else {
+    restoreArray = [];
+    reIndex = -1;
+  }
+}
