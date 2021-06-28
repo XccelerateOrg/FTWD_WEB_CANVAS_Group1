76ef7438de43ce5d8c85245b05876a6c8670187f
Zach Lalwani <zach.lalwani@gmail.com>
2021-06-28 09:02:41 +0800

    firebase alert and fixed erros

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -72,28 +72,29 @@ function server() {
     // var uploadTask = storageRef.child(`images/canvas.png`).put(blob);
     // var uploadTask = storageRef.child('images/canvas.jpg').put(blob);
     
-    uploadTask.on('state_changed',  (snapshot) => {
-        // Observe state change events such as progress, pause, and resume
-        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-        console.log('Upload is ' + progress + '% done');
-        switch (snapshot.state) {
-            case firebase.storage.TaskState.PAUSED: // or 'paused'
-                console.log('Upload is paused');
-                break;
-            case firebase.storage.TaskState.RUNNING: // or 'running'
-                console.log('Upload is running');
-                break;
-        }
-    },  (error) => {
-        // Handle unsuccessful uploads
-        console.log("error");
-    },  () => {
-        // Handle successful uploads on complete
-        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-        var downloadURL = uploadTask.snapshot.downloadURL;
-        console.log(uploadTask);
-        console.log(uploadTask.snapshot);
-        console.log('File available at', downloadURL);
+    // uploadTask.on('state_changed',  (snapshot) => {
+    //     // Observe state change events such as progress, pause, and resume
+    //     var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    //     console.log('Upload is ' + progress + '% done');
+    //     switch (snapshot.state) {
+    //         case firebase.storage.TaskState.PAUSED: // or 'paused'
+    //             console.log('Upload is paused');
+    //             break;
+    //         case firebase.storage.TaskState.RUNNING: // or 'running'
+    //             console.log('Upload is running');
+    //             break;
+    //     }
+    // },  (error) => {
+    //     // Handle unsuccessful uploads
+    //     console.log("error");
+    // },  () => {
+    //     // Handle successful uploads on complete
+    //     // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+    //     var downloadURL = uploadTask.snapshot.downloadURL;
+    //     console.log(uploadTask);
+    //     console.log(uploadTask.snapshot);
+    //     console.log('File available at', downloadURL);
 
-    });
+    // });
+    alert("Your File Is Available On Firebase");
 };
